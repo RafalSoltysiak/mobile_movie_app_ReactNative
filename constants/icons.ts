37dc65a032eec01834ea50980178a9e6ea80efce
MrBiedny <MrBiedny@gmail.com>
2025-03-11 17:41:53 +0100

    add routing and navigation

---
+++ constants/icons.ts
@@ -0,0 +1,19 @@
+import home from "@/assets/icons/home.png";
+import search from "@/assets/icons/search.png";
+import profile from "@/assets/icons/profile.png";
+import save from "@/assets/icons/save.png";
+// import arrow from "@/assets/icons/arrow.png";
+// import logo from "@/assets/icons/logo.png";
+// import play from "@/assets/icons/play.png";
+// import star from "@/assets/icons/star.png";
+
+export const icons = {
+  home,
+  search,
+  profile,
+  save,
+  // arrow,
+  // logo,
+  // play,
+  // star,
+};
