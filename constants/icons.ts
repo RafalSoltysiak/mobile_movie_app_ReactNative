972c70096c8f1d3896cd643eb1686261bd5814fe
MrBiedny <MrBiedny@gmail.com>
2025-03-11 18:01:01 +0100

    add home screen ui

--- constants/icons.ts
+++ constants/icons.ts
@@ -2,8 +2,8 @@ import home from "@/assets/icons/home.png";
 import search from "@/assets/icons/search.png";
 import profile from "@/assets/icons/profile.png";
 import save from "@/assets/icons/save.png";
+import logo from "@/assets/icons/logo1.png";
 // import arrow from "@/assets/icons/arrow.png";
-// import logo from "@/assets/icons/logo.png";
 // import play from "@/assets/icons/play.png";
 // import star from "@/assets/icons/star.png";
 
@@ -12,8 +12,8 @@ export const icons = {
   search,
   profile,
   save,
+  logo,
   // arrow,
-  // logo,
   // play,
   // star,
 };
