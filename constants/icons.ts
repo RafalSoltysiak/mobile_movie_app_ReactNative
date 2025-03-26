447b058fe102d252caba5afe76e7f88226ed296c
MrBiedny <MrBiedny@gmail.com>
2025-03-26 20:07:28 +0100

    add fetch & display movies

--- constants/icons.ts
+++ constants/icons.ts
@@ -3,9 +3,9 @@ import search from "@/assets/icons/search.png";
 import profile from "@/assets/icons/profile.png";
 import save from "@/assets/icons/save.png";
 import logo from "@/assets/icons/logo1.png";
+import star from "@/assets/icons/star.png";
 // import arrow from "@/assets/icons/arrow.png";
 // import play from "@/assets/icons/play.png";
-// import star from "@/assets/icons/star.png";
 
 export const icons = {
   home,
@@ -13,7 +13,7 @@ export const icons = {
   profile,
   save,
   logo,
+  star,
   // arrow,
   // play,
-  // star,
 };
